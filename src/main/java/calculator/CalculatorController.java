0ad3b20b9c7d31809508b0e4b97bae74876409dc
Patrik <patak1998@gmail.com>
2020-05-17 12:25:10 +0200

    finish the task

--- src/main/java/calculator/CalculatorController.java
+++ src/main/java/calculator/CalculatorController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+
 public class CalculatorController {
 
     @FXML
@@ -22,12 +23,28 @@ public class CalculatorController {
     }
 
     @FXML
+    private void processAC(ActionEvent event) {
+        String digitPressed = ((Button) event.getSource()).getText();
+        System.out.println(digitPressed);
+        calculator = new Calculator();
+        display.setText("0");
+    }
+
+    @FXML
+    public void processPM(ActionEvent event){
+        System.out.println("The sign of the number has changed");
+        double number = calculator.calculate(Double.parseDouble(display.getText()), -1, "*");
+        display.setText(String.valueOf(number));
+    }
+
+    @FXML
     public void processDigit(ActionEvent event) {
         String digitPressed = ((Button) event.getSource()).getText();
         System.out.println(digitPressed);
         if (startNumber || display.getText().equals("0")) {
             display.setText(digitPressed);
-        } else {
+        }
+        else {
             display.setText(display.getText() + digitPressed);
         }
         startNumber = false;
@@ -38,14 +55,15 @@ public class CalculatorController {
         String operatorPressed = ((Button) event.getSource()).getText();
         System.out.println(operatorPressed);
         if (operatorPressed.equals("=")) {
-           if (operator.isEmpty()) {
-               return;
-           }
-           double number2 = Double.parseDouble(display.getText());
-           double result = calculator.calculate(number1, number2, operator);
-           display.setText(String.format("%.0f", result));
-           operator = "";
-        } else {
+            if (operator.isEmpty()) {
+                return;
+            }
+            double number2 = Double.parseDouble(display.getText());
+            double result = calculator.calculate(number1, number2, operator);
+            display.setText(String.valueOf(result));
+            operator = "";
+        }
+        else {
             if (! operator.isEmpty()) {
                 return;
             }
@@ -54,5 +72,4 @@ public class CalculatorController {
             startNumber = true;
         }
     }
-
 }
